92943babffbe735e901838df47e0978133b08d64
Jacques <booysenjacques@gmail.com>
2015-06-09 18:56:53 +0200

    new

--- inst/htmlwidgets/DiagrammeR.js
+++ inst/htmlwidgets/DiagrammeR.js
@@ -104,9 +104,34 @@ HTMLWidgets.widget({
     // use this to sort of make our diagram responsive
     //  or at a minimum fit within the bounds set by htmlwidgets
     //  for the parent container
-    function makeResponsive(el){
+    function makeResponsive(el) {
        var svg = el.getElementsByTagName("svg")[0];
-       if(svg){
+       if (svg) {
+        
+        var svgContainer = d3.select("svg"),inner = svgContainer.select("g");
+
+       // Set up zoom support
+       var zoom = d3.behavior.zoom().on("zoom", function() {         
+         inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                  "scale(" + d3.event.scale + ")");
+       });
+       svgContainer.call(zoom);
+        
+        
+        var nodes = svgContainer.selectAll(".node")
+        var edgeLabels = svgContainer.selectAll(".edgeLabel").selectAll("span")
+                        
+        nodes.on("click", function() {          
+          Shiny.onInputChange('mermaidnode', this.id);          
+          d3.event.stopPropagation();
+        }); 
+        
+        edgeLabels.on("click", function() {                     
+          Shiny.onInputChange('mermaidedgelabel', this.innerHTML);      
+          d3.event.stopPropagation();
+        }); 
+        
+        
         if(svg.width) {svg.removeAttribute("width")};
         if(svg.height) {svg.removeAttribute("height")};
         svg.style.width = "100%";
